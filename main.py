cfde41cf4db8e1fb376fc64418c8cfc030379d01
Abhishek Choudhury <115447569+abhishekc-bhs@users.noreply.github.com>
2023-08-29 19:16:36 +0530

    nice

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ stub = modal.Stub(name="link-scraper")
 @stub.function()
 def do_something():
     import numpy as np
-    
+
     # Creating 5x4 array
     array = np.arange(20).reshape(5, 4)
     print(array)
     print()
-    
+
     # If no axis mentioned, then it works on the entire array
     print(np.argmax(array))
-    
+
     # If axis=1, then it works on each row
     print(np.argmax(array, axis=1))
-    
+
     # If axis=0, then it works on each column
     print(np.argmax(array, axis=0))
-
 
 
 @stub.local_entrypoint()
